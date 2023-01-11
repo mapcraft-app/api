a51cf672847e0f7e1971b039439fb7f03e5a18e8
c-bertran <clement.bertrand22@gmail.com>
2023-01-11 23:02:12 +0100

    Patch date for main comment

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,9 @@ import replace from '@rollup/plugin-replace';
 import tsConfigPaths from 'rollup-plugin-ts-paths';
 import json from '@rollup/plugin-json';
 import typescript from '@rollup/plugin-typescript';
-//import terser from '@rollup/plugin-terser';
+import terser from '@rollup/plugin-terser';
 
-const banner = '/**\n* @license\n* mapcraft-api\n* Copyright (C) 2021 - 2022 Clément Bertrand (https://github.com/c-bertran)\n*\n* This program is free software: you can redistribute it and/or modify\n* it under the terms of the GNU General Public License as published by\n* the Free Software Foundation, either version 3 of the License, or\n* (at your option) any later version.\n*\n* This program is distributed in the hope that it will be useful,\n* but WITHOUT ANY WARRANTY; without even the implied warranty of\n* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n* GNU General Public License for more details.\n*/';
+const banner = `/**\n* @license\n* mapcraft-api\n* Copyright (C) 2021 - ${new Date().getFullYear()} Clément Bertrand (https://github.com/c-bertran)\n*\n* This program is free software: you can redistribute it and/or modify\n* it under the terms of the GNU General Public License as published by\n* the Free Software Foundation, either version 3 of the License, or\n* (at your option) any later version.\n*\n* This program is distributed in the hope that it will be useful,\n* but WITHOUT ANY WARRANTY; without even the implied warranty of\n* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n* GNU General Public License for more details.\n*/`;
 
 export default {
 	external: ['7zip-min', 'better-sqlite3', 'crypto', 'events', 'fs', 'fs/promises', 'http', 'https', 'os', 'path', 'process'],
@@ -44,10 +44,11 @@ export default {
 		tsConfigPaths(),
 		json(),
 		typescript(),
-		/*terser({
+		terser({
+			compress: process.env.DEV ?? false,
 			format: {
 				comments: 'some'
 			}
-		})*/
+		})
 	]
 };
