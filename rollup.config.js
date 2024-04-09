a72a825f6e8c0427bcdd774103f503287dfcb1db
c-bertran <clement.bertrand22@gmail.com>
2024-04-09 18:55:17 +0200

    Update sql

--- rollup.config.js
+++ rollup.config.js
@@ -3,15 +3,15 @@ import json from '@rollup/plugin-json';
 import typescript from '@rollup/plugin-typescript';
 import copy from 'rollup-plugin-copy';
 import terser from '@rollup/plugin-terser';
-import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+// import commonjs from '@rollup/plugin-commonjs';
+// import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 const banner = `/**\n* @license\n* mapcraft-api\n* Copyright (C) 2021 - ${new Date().getFullYear()} Clément Bertrand (https://github.com/c-bertran)\n*\n* This program is free software: you can redistribute it and/or modify\n* it under the terms of the GNU General Public License as published by\n* the Free Software Foundation, either version 3 of the License, or\n* (at your option) any later version.\n*\n* This program is distributed in the hope that it will be useful,\n* but WITHOUT ANY WARRANTY; without even the implied warranty of\n* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n* GNU General Public License for more details.\n*/`;
 
 export default [
 	{
 		external: [
-			'7zip-bin', 'child_process', 'crypto', 'events', 'fs',
+			'7zip-bin', 'better-sqlite3', 'child_process', 'crypto', 'events', 'fs',
 			'fs/promises', 'http', 'https', 'os', 'path', 'process',
 			'prompts', 'url'
 		],
@@ -42,14 +42,14 @@ export default [
 			}),
 			json(),
 			typescript(),
-			commonjs(),
-			nodeResolve(),
-			terser({
-				compress: (process.env.DEV !== true),
-				format: {
-					comments: 'some'
-				}
-			}),
+			(process.env.DEV.localeCompare('true') !== 0)
+				?	terser({
+						compress: true,
+						format: {
+							comments: 'some'
+						}
+					})
+				: undefined,
 			copy({
 				targets: [
 					{
--- src/backend/sql.ts
+++ src/backend/sql.ts
@@ -12,23 +12,18 @@ export default class {
 		verb: ((message: any, ...optional: any[]) => void) | undefined = undefined,
 		tables: tableInterface | tableInterface[] | undefined = undefined
 	) {
-		const toto = resolve(env.save, name, 'mapcraft.db');
-		console.log(toto);
 		this.db = new databaseConstrutor(
-			toto,
+			resolve(env.save, name, 'mapcraft.db'),
 			{ timeout: 250, verbose: verb }
 		);
-		console.log('one');
 		if (tables) {
 			this.tables = Array.isArray(tables)
 				? tables
 				: [ tables ];
 		} else
 			this.tables = [];
-		console.log('two');
 		for (const table of this.tables)
 			this.db.exec(table.sql);
-		console.log('three');
 	}
 
 	isExistTable(names: string | string[]): number | number[] {
